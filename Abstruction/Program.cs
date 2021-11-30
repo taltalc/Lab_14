8d70644054c54ad0af0f46668cd2501da4400240
Ольга Черепанова <chernichka.ekb@gmail.com>
2021-11-30 10:46:12 +0500

    Добавьте файлы проекта.

---
+++ Abstruction/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstruction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.ShowInfo();
+            Dog dog = new Dog();
+            dog.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public Animal()
+        {
+           
+        }
+        public abstract void Say();
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+    }
+      class Cat:Animal
+      {
+        string name="Кошка";
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+   
+      }
+    class Dog : Animal
+    {
+        string name = "Собака";
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Гав");
+        }
+
+    }
+
+
+
+}
